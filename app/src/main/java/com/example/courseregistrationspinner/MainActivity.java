b673a43d4a4375a3509ab34e511230f83f4ca927
leonard0606 <leonardkoechk@gmail.com>
2020-09-16 17:57:11 +0300

    Android Studio Spinner

---
+++ app/src/main/java/com/example/courseregistrationspinner/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.courseregistrationspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.layout.simple_spinner_item;
+
+public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener{
+    TextView textView;
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =findViewById(R.id.textView);
+
+        spinner = findViewById(R.id.spinner);
+//        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this,
+                R.array.Spinner_Items,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
